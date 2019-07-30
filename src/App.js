7c223ead33019879a97d93cb8c8fde7294cc9e95
bvasilop <bvasilop@gmail.com>
2019-07-29 20:31:17 -0700

    added card and draw button components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card/Card';
+import Draw from './Draw/Draw';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.updateCard = this.updateCard.bind(this);
+
+    this.state = {
+      cards: [
+        {id: 1, eng: "English", han: "Hanzi",  pin: "Pinyin"},
+        {id: 2, eng: "English_2", han: "Hanzi_2",  pin: "Pinyin_2"},
+        {id: 3, eng: "English_3", han: "Hanzi_3",  pin: "Pinyin_3"},
+        {id: 4, eng: "English_4", han: "Hanzi_4",  pin: "Pinyin_4"},
+      ],
+      currentCard: {}
+    }
+  }
+
+    componentWillMount() {
+      const currentCards = this.state.cards;
+
+      this.setState({
+        cards: currentCards,
+        currentCard: this.getRandomCard(currentCards)
+      })
+    }
+
+    getRandomCard(currentCards) {
+      const card = currentCards[Math.floor(Math.random()* currentCards.length)]
+      return card;
+
+    }
+
+    updateCard() {
+      const currentCards = this.state.cards;
+      this.setState({
+        currentCard: this.getRandomCard(currentCards)
+      })
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="cardRow">
+          <Card eng={this.state.currentCard.eng}
+                han={this.state.currentCard.han}
+                pin={this.state.currentCard.pin}
+                />
+          </div>
+          <div className="buttonRow">
+            <Draw drawCard={this.updateCard}/>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Draw/Draw.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Draw.css';
+
+
+export default class Draw extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.drawCard = this.drawCard.bind(this)
+  }
+
+  drawCard() {
+    this.props.drawCard();
+  }
+  render(props) {
+    return (
+      <div className="buttonContainer">
+        <button className="btn" onClick={this.drawCard}>Draw Card</button>
+      </div>
+    )
+  }
+}
