4c1cb8f3c29eff32c7b5c22c478c6d7d49c0e9e9
bvasilop <bvasilop@gmail.com>
2020-03-19 21:14:20 -0700

    updated dependencies and add eslint config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
+import Loader from 'react-loader';
+import firebase from 'firebase/app';
 import Card from './Card/Card';
 import Draw from './Draw/Draw';
-import Loader from 'react-loader';
 import { DB_CONFIG } from './Config/Firebase/db_config.js';
-import firebase from 'firebase/app';
 import 'firebase/database';
 
 class App extends React.Component {
@@ -12,64 +12,70 @@ class App extends React.Component {
     super(props);
 
     this.app = firebase.initializeApp(DB_CONFIG);
-    this.database = this.app.database().ref().child('cards');
+    this.database = this.app
+      .database()
+      .ref()
+      .child('cards');
     this.updateCard = this.updateCard.bind(this);
 
     this.state = {
-      cards: [
-      ],
+      cards: [],
       currentCard: {},
-      loaded: false
-    }
+      loaded: false,
+    };
   }
 
-    componentDidMount = () => {
-      const currentCards = this.state.cards;
+  componentDidMount = () => {
+    const { cards } = this.state;
+    const currentCards = cards;
 
-      this.database.on('child_added', snap => {
-        currentCards.push({
-          id: snap.key,
-          sta: snap.val().sta,
-          cap:  snap.val().cap,
-          img:  snap.val().img
-        })
+    this.database.on('child_added', snap => {
+      currentCards.push({
+        id: snap.key,
+        sta: snap.val().sta,
+        cap: snap.val().cap,
+        img: snap.val().img,
+      });
 
-        this.setState({
-          cards: currentCards,
-          currentCard: this.getRandomCard(currentCards),
-          loaded: true,
-        })
-      })
-    }
-
-    getRandomCard = (currentCards) => {
-      const card = currentCards[Math.floor(Math.random()* currentCards.length)]
-      return card;
-    }
-
-    updateCard = () => {
-      const currentCards = this.state.cards;
       this.setState({
-        currentCard: this.getRandomCard(currentCards)
-      })
-    }
+        cards: currentCards,
+        currentCard: this.getRandomCard(currentCards),
+        loaded: true,
+      });
+    });
+  };
+
+  getRandomCard = currentCards => {
+    const card = currentCards[Math.floor(Math.random() * currentCards.length)];
+    return card;
+  };
+
+  updateCard = () => {
+    const { cards } = this.state;
+    const currentCards = cards;
+    this.setState({
+      currentCard: this.getRandomCard(currentCards),
+    });
+  };
 
   render() {
+    const { loaded, currentCard } = this.state;
     return (
       <div className="App">
-        <Loader loaded={this.state.loaded} top="35%" scale={1.25}>
-        <div className="cardRow">
-            <Card sta={this.state.currentCard.sta}
-                  cap={this.state.currentCard.cap}
-                  img={this.state.currentCard.img}
-                  />
-            </div>
-            <div className="buttonRow">
-              <Draw drawCard={this.updateCard}/>
-            </div>
-            </Loader>
-        </div>
-    )
+        <Loader loaded={loaded} top="35%" scale={1.25}>
+          <div className="cardRow">
+            <Card
+              sta={currentCard.sta}
+              cap={currentCard.cap}
+              img={currentCard.img}
+            />
+          </div>
+          <div className="buttonRow">
+            <Draw drawCard={this.updateCard} />
+          </div>
+        </Loader>
+      </div>
+    );
   }
 }
 
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Card.css';
 
 const Card = ({ sta, cap, img }) => (
@@ -8,20 +9,26 @@ const Card = ({ sta, cap, img }) => (
         <div className="sta">
           <h1>what is the capital of</h1>
           {sta}
-          <br/>
-          <img src={img} alt="state pic"></img>
+          <br />
+          <img src={img} alt="state pic" />
         </div>
       </div>
       <div className="front back">
         <div className="cap">
-        <h1>the capital is</h1>
-        {cap}
-        <br/>
-        <img src={img} alt="state pic"></img>
+          <h1>the capital is</h1>
+          {cap}
+          <br />
+          <img src={img} alt="state pic" />
         </div>
       </div>
     </div>
   </div>
-)
+);
+
+Card.propTypes = {
+  sta: PropTypes.string,
+  cap: PropTypes.string,
+  img: PropTypes.string,
+};
 
 export default Card;
