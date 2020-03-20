dd8e8d055e3606d777a68e020412e28160f42695
bvasilop <bvasilop@gmail.com>
2020-03-19 21:22:29 -0700

    Updates

---
+++ precache-manifest.e2b9be6f619007e424c82448387f9d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af969b6ca07582d0e8939042fc69c22d",
+    "url": "/bvasilop/react-flashcard-app/index.html"
+  },
+  {
+    "revision": "2dca389230cc7bbe46af",
+    "url": "/bvasilop/react-flashcard-app/static/css/main.228957f9.chunk.css"
+  },
+  {
+    "revision": "760c9a27b1ab8437a4ef",
+    "url": "/bvasilop/react-flashcard-app/static/js/2.422d9be4.chunk.js"
+  },
+  {
+    "revision": "052157e0581e32a2b8fa8f7f1414d257",
+    "url": "/bvasilop/react-flashcard-app/static/js/2.422d9be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dca389230cc7bbe46af",
+    "url": "/bvasilop/react-flashcard-app/static/js/main.d17e1455.chunk.js"
+  },
+  {
+    "revision": "9b51ae256f8c0f08075d",
+    "url": "/bvasilop/react-flashcard-app/static/js/runtime-main.c9625918.js"
+  }
+]);
